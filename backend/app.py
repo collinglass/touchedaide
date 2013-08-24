d32f5e777708d4c458092ea0cb2bf10e9e59c3da
Philip Schleihauf <uniphil@gmail.com>
2013-08-24 18:14:51 -0400

    fake data server

--- backend/app.py
+++ backend/app.py
@@ -8,13 +8,36 @@
    Data interface for ToucheAide
 """
 
-from flask import Flask
+from flask import Flask, Blueprint, json, Response
 
 app = Flask(__name__)
 
-@app.route('/api/')
-def api_root():
-    return "hellooooooo"
+
+
+
+
+fake_api = Blueprint('api', 'api')
+
+@fake_api.route('/')
+def fake_index():
+    return 'hellooooooooo'
+
+
+@fake_api.route('/activities/')
+def fake_activities_index():
+    with open('resources/samples.json') as fake_file:
+        return Response(fake_file.read(), mimetype='application/json')
+
+
+@fake_api.route('/activities/<string:iati_code>')
+def fake_activity(iati_code):
+    with open('resources/{}.json'.format(iati_code)) as fake_file:
+        return Response(fake_file.read(), mimetype='application/json')
+
+
+
+app.register_blueprint(fake_api, url_prefix='/api')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
